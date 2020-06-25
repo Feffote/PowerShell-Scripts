7973c24e243605e8c57eeab1b4e624f23bcb7da4
Feffote <zinnifederico@gmail.com>
2020-06-25 13:20:40 -0300

    Add files via upload

---
+++ getNames.ps1
@@ -0,0 +1,11 @@
+﻿#pasar archivo con lista de dominios y IDs; ej EMPRESA\500720321
+$users = Get-Content 'C:\[...]\Users.txt' #pasar ruta hacia el archivo .txt
+
+$nameList = @()
+foreach($user in $users){
+    $dominio, $ID = $user.Split('\')
+    $dominio += '.techint.net'
+    $nameList += Get-ADUser $ID -Server $dominio -Properties mail | Select-Object samaccountname,name,mail 
+}
+
+$nameList | export-csv -Path .\nameList.csv -NoTypeInformation
