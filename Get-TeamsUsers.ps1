27a58c9880c1ba3a0ea80c75d4d1dd5e952b816e
Feffote <zinnifederico@gmail.com>
2020-06-25 15:06:30 -0300

    Add files via upload

---
+++ Get-TeamsUsers.ps1
@@ -0,0 +1,8 @@
+﻿#get list of users in a Microsoft Teams group
+
+#connect module and enter credentials
+Connect-MicrosoftTeams
+#get ID of the groups to which a user belongs
+Get-Team -User #--- your email here
+#pass desired group ID
+Get-TeamUser -GroupId [...Group ID here...] | Select-Object User | Export-Csv -Path .\Desktop\TeamsUsers.csv -NoTypeInformation
